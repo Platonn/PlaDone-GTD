ce7b52eadaa6d00390be61aec0bc0074459d1477
Platonn <platonn@platonn.com>
2014-11-04 04:40:08 +0100

    Color and icon added to Context model

--- db/migrate/20141104024706_create_contexts.rb
+++ db/migrate/20141104024706_create_contexts.rb
@@ -2,6 +2,8 @@ class CreateContexts < ActiveRecord::Migration
   def change
     create_table :contexts do |t|
       t.string :name
+      t.string :color
+      t.string :icon
 
       t.timestamps
     end
