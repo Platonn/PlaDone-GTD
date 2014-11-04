fa3c962c83d3bca350cceaf6ec983daee6ce8180
Platonn <platonn@platonn.com>
2014-11-04 21:50:01 +0100

    Fixbug, fix #12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -71,6 +71,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :done, :deadline, :time_needed, :project_id, :context_id)
+      params.require(:task).permit(:name, :done, :deadline, :time_needed, :project_id, :context_id, :time_needed, :postponed_to)
     end
 end
