3f2a574631e372fb5ab0d415d0e007e86ff1c88a
Platonn <platonn@platonn.com>
2014-11-04 03:07:55 +0100

    Db seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ProjectCategory.create([
+    {name: 'Osobiste'},
+    {name: 'Studia'}
+                       ])
+Project.create([
+    {name: 'Zadanie z Ruby on Rails', project_category_id: 2},
+    {name: 'Zadanie z PGK', project_category_id: 2},
+               ])
+
+Context.create([
+    {name: 'Rozmowy telefoniczne'},
+    {name: 'W domu'},
+    {name: 'W podróży'},
+    {name: 'W instytucie informatyki'},
+    {name: 'Na laptopie'},
+    {name: 'Na komputerze'},
+    {name: 'Online'},
+    {name: 'Gdziekolwiek'}
+               ])
+
+Task.create([
+    {name: 'Przeczytać treść zadania z RoR.', project_id: 1, context_id: 7},
+    {name: 'Zaprogramować zadanie z RoR.', project_id: 1, context_id: 5},
+    {name: 'Przeczytać treść zadania z PGK.', project_id: 2, context_id: 7},
+    {name: 'Zaprogramować zadanie z PGK.', project_id: 2, context_id: 5},
+            ])
