acb49c7a0623d1ac4697e5cb9824c92fd3961c31
Platonn <platonn@platonn.com>
2014-11-04 04:49:44 +0100

    Deadline added to Project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104033537) do
+ActiveRecord::Schema.define(version: 20141104034625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141104033537) do
     t.integer  "project_category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "deadline"
   end
 
   create_table "tasks", force: true do |t|
