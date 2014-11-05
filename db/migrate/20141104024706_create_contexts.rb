5f58da1f15b82ed6750827d87e3e009a965d9b39
Platonn <platonn@platonn.com>
2014-11-05 03:59:48 +0100

    Reparing migrations, try 1

--- db/migrate/20141104024706_create_contexts.rb
+++ db/migrate/20141104024706_create_contexts.rb
@@ -2,9 +2,6 @@ class CreateContexts < ActiveRecord::Migration
   def change
     create_table :contexts do |t|
       t.string :name
-      t.string :color
-      t.string :icon
-
       t.timestamps
     end
   end
