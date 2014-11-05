0424061a435a9031d8a740875a434fa3b48d78b1
Platonn <platonn@platonn.com>
2014-11-05 04:01:50 +0100

    Repairing migrations, try 2

--- db/migrate/20141104024706_create_contexts.rb
+++ db/migrate/20141104024706_create_contexts.rb
@@ -2,6 +2,9 @@ class CreateContexts < ActiveRecord::Migration
   def change
     create_table :contexts do |t|
       t.string :name
+      t.string :color
+      t.string :icon
+
       t.timestamps
     end
   end
--- db/migrate/20141104033537_add_color_and_icon_to_context.rb
+++
@@ -1,6 +0,0 @@
-class AddColorAndIconToContext < ActiveRecord::Migration
-  def change
-    add_column :contexts, :color, :string
-    add_column :contexts, :icon, :string
-  end
-end
