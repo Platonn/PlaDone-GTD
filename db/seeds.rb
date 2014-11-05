fc040c6cc2488485bf1524981b3400bf0b383c60
Platonn <platonn@platonn.com>
2014-11-05 03:52:30 +0100

    Seeds dumped from current database state

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-ProjectCategory.create([
-    {name: 'Osobiste'},
-    {name: 'Studia'},,
-    {name: 'Kiedyś/Może'}
-                       ])
-
-Project.create([
-    {name: 'Zadanie z Ruby on Rails', project_category_id: 2},
-    {name: 'Zadanie z PGK', project_category_id: 2},
-               ])
-
-Context.create([
-    {name: 'Rozmowy telefoniczne'},
-    {name: 'W domu'},
-    {name: 'W podróży'},
-    {name: 'W instytucie informatyki'},
-    {name: 'Na laptopie'},
-    {name: 'Na komputerze'},
-    {name: 'Online'},
-    {name: 'Gdziekolwiek'},
-               ])
-
-Task.create([
-    {name: 'Przeczytać treść zadania z RoR.', project_id: 1, context_id: 7},
-    {name: 'Zaprogramować zadanie z RoR.', project_id: 1, context_id: 5},
-    {name: 'Przeczytać treść zadania z PGK.', project_id: 2, context_id: 7},
-    {name: 'Zaprogramować zadanie z PGK.', project_id: 2, context_id: 5},
-            ])
+Context.create!([
+  {name: "spam", color: "", icon: "", deleted_at: "2000-01-01 20:29:32"},
+  {name: "rt", color: "", icon: "", deleted_at: "2000-01-01 20:39:17"},
+  {name: "Rozmowy telefoniczne", color: "lightgreen", icon: "earphone", deleted_at: nil},
+  {name: "W domu", color: "lightsalmon", icon: "home", deleted_at: nil},
+  {name: "Gdziekolwiek", color: "lightblue", icon: "globe", deleted_at: nil},
+  {name: "W podróży", color: "navajowhite", icon: "briefcase", deleted_at: nil},
+  {name: "Online", color: "greenyellow", icon: "flash", deleted_at: nil},
+  {name: "Na komputerze", color: "mintcream", icon: "hdd", deleted_at: nil},
+  {name: "W instytucie informatyki", color: "lightpink", icon: "bell", deleted_at: nil},
+  {name: "Agendy", color: "paleturquoise", icon: "user", deleted_at: nil},
+  {name: "Na laptopie", color: "lightgoldenrodyellow", icon: "floppy-disk", deleted_at: nil}
+])
+Project.create!([
+  {name: "Zadanie z Ruby on Rails", project_category_id: 2, deadline: "2014-11-09 21:59:00", deleted_at: nil, notes: nil},
+  {name: "Zadanie z PGK", project_category_id: 2, deadline: "2014-11-04 11:30:00", deleted_at: nil, notes: nil}
+])
+ProjectCategory.create!([
+  {name: "Osobiste", deleted_at: nil},
+  {name: "Studia", deleted_at: nil},
+  {name: "Kiedyś/Może", deleted_at: nil}
+])
+Task.create!([
+  {name: "Przeczytać treść zadania", done: true, deadline: nil, project_id: 1, context_id: 7, time_needed: "15 min", postponed_to: nil, deleted_at: nil},
+  {name: "asd", done: false, deadline: nil, project_id: nil, context_id: nil, time_needed: "", postponed_to: nil, deleted_at: "2000-01-01 01:44:39"},
+  {name: "Zaprogramować zadanie", done: false, deadline: nil, project_id: 1, context_id: 5, time_needed: "", postponed_to: nil, deleted_at: "2000-01-01 01:44:52"},
+  {name: "Zrobić podstawową aplikację", done: false, deadline: nil, project_id: 1, context_id: 5, time_needed: "4 h", postponed_to: nil, deleted_at: nil},
+  {name: "Dostosować flow usera", done: false, deadline: nil, project_id: 1, context_id: 5, time_needed: "2 h", postponed_to: nil, deleted_at: nil},
+  {name: "Zaprogramować zadanie", done: false, deadline: nil, project_id: 2, context_id: 5, time_needed: "16 h", postponed_to: nil, deleted_at: nil},
+  {name: "Przeczytać treść zadania", done: false, deadline: nil, project_id: 2, context_id: 7, time_needed: "10 min", postponed_to: nil, deleted_at: nil},
+  {name: "Zainstalować devise", done: false, deadline: nil, project_id: 1, context_id: 5, time_needed: "1 h", postponed_to: nil, deleted_at: nil}
+])
