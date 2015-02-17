c2382ff91379b000837c1391a4c51e352f6ca4e3
Platonn <platonn@platonn.com>
2015-02-17 22:57:53 +0100

    Refactored TaskService and TaskController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   #load_and_authorize_resource
+  before_action :set_task_id
   before_action :set_task_service
-  before_action :set_task, only: [:show, :edit, :update, :destroy, :toggle_done]
+  before_action :set_task, only: [:show, :edit]
   before_action :get_active_contexts, only: [:new, :edit]
   before_action :get_active_projects, only: [:new, :edit]
 
   def index
-    @tasks = @task_service.get_active_tasks_of_current_user
+    @tasks = @task_service.get_active_tasks_of(current_user)
   end
 
   def show
@@ -20,7 +21,7 @@ class TasksController < ApplicationController
   end
 
   def toggle_done
-    if @task_service.toggle_done(@task)
+    if @task_service.toggle_done(@task_id, current_user)
       render :json => {:error => 0, :success => 1}
     else
       render :status => 500
@@ -41,7 +42,7 @@ class TasksController < ApplicationController
   def update
     task_form = TaskForm.new(task_params)
     respond_to do |format|
-      if @task_service.update(@task, task_form)
+      if @task_service.update(@task_id, task_form, current_user)
         format.html { redirect_to tasks_url, notice: 'Task was successfully updated.' }
       else
         format.html { render :edit }
@@ -51,36 +52,38 @@ class TasksController < ApplicationController
 
   def destroy
     ###Soft delete:
-    @task_service.soft_delete(@task)
+    @task_service.soft_delete(@task_id, current_user)
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
+private
+  # Use callbacks to share common setup or constraints between actions.
 
-    def set_task_service
-      @task_service = TaskService.new(current_user)
-    end
+  def set_task_service
+    @task_service = TaskService.new
+  end
 
-    def set_task
-      @task = @task_service.get_task_by_id(params[:id])
-      if @task.nil?
-        raise ActionController::RoutingError.new('Given Task is null')
-      end
-    end
+  def set_task_id
+    @task_id = params[:id]
+  end
 
-    def get_active_contexts
-      @active_contexts = current_user.contexts.active
-    end
+  def set_task
+    @task = @task_service.get_task_by_id(@task_id, current_user)
+    raise ActionController::RoutingError.new('Given Task is null') if @task.nil?
+  end
 
-    def get_active_projects
-      @active_projects = current_user.projects.active
-    end
+  def get_active_contexts
+    @active_contexts = current_user.contexts.active
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
-      params.require(:task).permit(:name, :done, :deadline, :time_needed, :project_id, :context_id, :time_needed, :postponed_to)
-    end
+  def get_active_projects
+    @active_projects = current_user.projects.active
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def task_params
+    params.require(:task).permit(:name, :done, :deadline, :time_needed, :project_id, :context_id, :time_needed, :postponed_to)
+  end
 end
--- app/services/task_service.rb
+++ app/services/task_service.rb
@@ -1,18 +1,10 @@
 class TaskService
-  def initialize(current_user)
-    @current_user = current_user
+  def get_task_by_id(task_id, current_user)
+    task = find_task_by_id(task_id)
+    current_user.can_use?(task) ? task : nil
   end
 
-  def get_task_by_id(task_id)
-    task = Task.find(task_id)
-    current_user_can_use_task?(task) ? task : nil
-  end
-
-  def get_active_tasks_of_current_user
-    get_active_tasks_of_user(@current_user)
-  end
-
-  def get_active_tasks_of_user(user)
+  def get_active_tasks_of(user)
     user.tasks.active.order(:done,
                             :context_id,
                             :deadline,
@@ -20,32 +12,30 @@ class TaskService
                             created_at: :desc)
   end
 
-  def create(task_form, owner_user)
+  def create(task_form, creator_user)
     task_form.validate!
-    task = owner_user.tasks.build(task_form.attributes)
-    current_user_can_use_task?(task) ? task.save : nil
+    creator_user.tasks.build(task_form.attributes)
   end
 
-  def update(task, task_form)
+  def update(task_id, task_form, current_user)
     task_form.validate!
-    current_user_can_use_task?(task) ? task.update(task_form.attributes) : nil
+    task = find_task_by_id(task_id)
+    current_user.can_use?(task) ? task.update(task_form.attributes) : nil
   end
 
-  def soft_delete(task)
-    current_user_can_use_task?(task) ? task.soft_delete : nil
+  def soft_delete(task_id, current_user)
+    task = find_task_by_id(task_id)
+    current_user.can_use?(task) ? task.soft_delete : nil
   end
 
-  def toggle_done(task)
+  def toggle_done(task_id, current_user)
+    task = find_task_by_id(task_id)
     task.done != task.done
-    current_user_can_use_task?(task) ? task.save : nil
+    current_user.can_use?(task) ? task.save : nil
   end
 
-  private
-    def current_user_can_use_task?(task)
-      user_can_use_task?(@current_user, task)
-    end
-
-    def user_can_use_task?(user, task)
-      user.has_role?(:admin) || user.owns?(task)
-    end
+private
+  def find_task_by_id(task_id)
+    task = Task.find(task_id)
+  end
 end
