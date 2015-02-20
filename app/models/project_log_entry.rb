253b1a24fef2029c45ab6df0ab268159cb896320
Platonn <platonn@platonn.com>
2015-02-20 19:12:59 +0100

    Added associations to ProjectLogEntry

--- app/models/project_log_entry.rb
+++ app/models/project_log_entry.rb
@@ -1,4 +1,8 @@
 class ProjectLogEntry < ActiveRecord::Base
+  belongs_to  :project
+  belongs_to  :user
+  belongs_to  :task
+
   enum activity_type: [:project_created,
                       :project_deleted,
                       :project_closed,
