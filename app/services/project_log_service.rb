f26f3dab0c30dd552c79785bc9f7d901ae1e656d
Platonn <platonn@platonn.com>
2015-02-20 21:25:04 +0100

    Added ProjectLogService

---
+++ app/services/project_log_service.rb
@@ -0,0 +1,104 @@
+class ProjectLogService
+  def project_updated(project_id, user_id, previous_project, updated_project)
+    if previous_project.name != updated_project.name
+      project_name_changed(project_id, user_id, previous_project.name, updated_project.name)
+    end
+    if previous_project.project_category_id != updated_project.project_category_id
+      project_category_changed(project_id, user_id, previous_project.project_category_id, updated_project.project_category_id)
+    end
+    if previous_project.notes != updated_project.notes
+      project_notes_changed(project_id, user_id, previous_project.notes, updated_project.notes)
+    end
+    if previous_project.deadline != updated_project.deadline
+      project_deadline_changed(project_id, user_id, previous_project.deadline, updated_project.deadline)
+    end
+  end
+
+  def project_created(project_id, user_id, project_name)
+      ProjectLogEntry.create!(
+          :project_id => project_id,
+          :user_id => user_id,
+          :activity_type => :project_created,
+          :new_value => project_name)
+  end
+
+  def project_deleted(project_id, user_id)
+    ProjectLogEntry.create!(
+      :project_id => project_id,
+      :user_id => user_id,
+      :activity_type => :project_deleted)
+  end
+
+  def project_closed(project_id, user_id)
+    ProjectLogEntry.create!(
+      :project_id => project_id,
+      :user_id => user_id,
+      :activity_type => :project_closed)
+  end
+
+  def project_reopened(project_id, user_id)
+    ProjectLogEntry.create!(
+      :project_id => project_id,
+      :user_id => user_id,
+      :activity_type => :project_reopened)
+  end
+
+
+  def project_name_changed(project_id, user_id, previous_name, new_name)
+    ProjectLogEntry.create!(
+      :project_id => project_id,
+      :user_id => user_id,
+      :activity_type => :project_name_changed,
+      :previous_value => previous_name,
+      :new_value => new_name)
+  end
+
+  def project_category_changed(project_id, user_id, previous_project_category_id, updated_project_category_id)
+    ProjectLogEntry.create!(
+        :project_id => project_id,
+        :user_id => user_id,
+        :activity_type => :project_category_changed,
+        :previous_value => previous_project_category_id,
+        :new_value => updated_project_category_id)
+  end
+
+  def project_notes_changed(project_id, user_id, previous_notes, new__notes)
+    ProjectLogEntry.create!(
+        :project_id => project_id,
+        :user_id => user_id,
+        :activity_type => :project_notes_changed,
+        :previous_value => previous_notes,
+        :new_value => new__notes)
+  end
+
+  def project_deadline_changed(project_id, user_id, previous_deadline, new_deadline)
+    ProjectLogEntry.create!(
+        :project_id => project_id,
+        :user_id => user_id,
+        :activity_type => :project_deadline_changed,
+        :previous_value => previous_deadline,
+        :new_value => new_deadline)
+  end
+
+end
+
+=begin
+t.integer  "project_id",     null: false
+t.integer  "user_id",        null: false
+t.integer  "activity_type",  null: false
+t.integer  "task_id"
+t.string   "previous_value"
+t.string   "new_value"
+t.datetime "created_at"
+t.datetime "updated_at"
+
+  enum activity_type: [:project_created,
+                       :project_deleted,
+                       :project_closed,
+                       :project_reopened,
+                       :project_name_changed,
+                       :project_category_changed,
+                        :project_notes_changed,
+                        :project_deadline_changed
+  ]
+=end
