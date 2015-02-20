672b8a51357fac710a9fb0f4d70ab54def76fd7a
Platonn <platonn@platonn.com>
2015-02-20 21:19:59 +0100

    Expanded ProjLogEntr activity_type enum list

--- app/models/project_log_entry.rb
+++ app/models/project_log_entry.rb
@@ -4,10 +4,12 @@ class ProjectLogEntry < ActiveRecord::Base
   belongs_to  :task
 
   enum activity_type: [:project_created,
-                      :project_deleted,
-                      :project_closed,
-                      :project_reopened,
-                      :project_name_changed,
-                      :project_category_changed
-                     ]
+                       :project_deleted,
+                       :project_closed,
+                       :project_reopened,
+                       :project_name_changed,
+                       :project_category_changed,
+                       :project_notes_changed,
+                       :project_deadline_changed
+                      ]
 end
